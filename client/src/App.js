bca5837d2af34ef81ca9e59d060e66676b651a29
delanobmarques <delanobmarques@gmail.com>
2022-07-26 20:01:34 -0300

    create basic components structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import { AppBar , Container, Grid, Grow, Typography } from '@mui/material';
+
+import memories from './assets/memories.png';
+import Posts from './components/Posts/posts.component';
+import Form from './components/Form/form.component';
+import './styles.css';
 
 const App = () => {
+
+
   return (
-    <h1>App</h1>
+    <Container maxWidth="lg">
+        <AppBar className='app-bar' position='static' color='inherit'>
+            <Typography className='app-bar__heading' variant='h2' align='center'>
+                Memories
+            </Typography>
+            <img src={memories} alt="memories" height="60"/>
+        </AppBar>
+        <Grow in>
+            <Container>
+                <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                    <Grid item xs={12} sm={7}>
+                        <Posts/>
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <Form />
+                    </Grid>
+                </Grid>
+            </Container>
+        </Grow>
+
+    </Container>
   )
 }
 
